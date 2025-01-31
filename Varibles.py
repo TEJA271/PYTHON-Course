2aaab582f9d26ad96ecc1d86df1ecab6592eac86
TEJA271 <navatejtummalapally02@gmail.com>
2025-01-31 11:31:28 +0300

    Add files via upload

---
+++ Varibles.py
@@ -0,0 +1,17 @@
+a=2014
+#variables
+print(a)
+
+#string
+
+b="Navatej"
+print(b)
+
+name =input("Hey What is your name: ")
+
+print("Hey",name, "welcome to python " )
+
+grade = input("which grade are you studying :")
+
+print("WOW you are studying in ",grade)
+
