289cc8b68beb542e85ba99383dfa0d23bed9ffbf
TEJA271 <navatejtummalapally02@gmail.com>
2025-02-07 17:09:56 +0300

    Storing Birthdays.py

--- Storing Birthdays.py
+++ Storing Birthdays.py
@@ -1,15 +1,4 @@
-name = input("please enter your name : ")
+name=input("enter your Freind name:")
+birthday=input("enter your freind birthday :")
 
-print =input("enter your 1st freind name1: ")
-print =input("enter your 2nd freind name2: ")
-print =input("enter your 3rd freind name3: ")
-print =input("enter your 4th freind name4: ")
-print =input("enter your 5th freind name5: ")
-
-print=input("enter 1st freind Birthday date : ")
-print=input("enter 2nd freind Birthday date : ")
-print=input("enter 3rd freind Birthday date : ")
-print=input("enter 4th freind Birthday date : ")
-print=input("enter 5th freind Birthday date : ")
-
-print=input("your 1st freind name is",name1)
+print(name,birthday)
