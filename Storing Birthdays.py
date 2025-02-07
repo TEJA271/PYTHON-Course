80becae66edd8b5d7c7c2aa3b790c9263f679a1b
TEJA271 <navatejtummalapally02@gmail.com>
2025-02-07 14:29:35 +0300

    Add files via upload

---
+++ Storing Birthdays.py
@@ -0,0 +1,15 @@
+name = input("please enter your name : ")
+
+print =input("enter your 1st freind name1: ")
+print =input("enter your 2nd freind name2: ")
+print =input("enter your 3rd freind name3: ")
+print =input("enter your 4th freind name4: ")
+print =input("enter your 5th freind name5: ")
+
+print=input("enter 1st freind Birthday date : ")
+print=input("enter 2nd freind Birthday date : ")
+print=input("enter 3rd freind Birthday date : ")
+print=input("enter 4th freind Birthday date : ")
+print=input("enter 5th freind Birthday date : ")
+
+print=input("your 1st freind name is",name1)
