a32b922827118dcd050aba5d39a5e43884059ccb
TEJA271 <navatejtummalapally02@gmail.com>
2025-02-08 11:45:09 +0300

    Add files via upload

---
+++ Profit.py
@@ -0,0 +1,8 @@
+actual_cost=float(input("Please enter the actual cost price : "))
+sale_amount=float(input("enter the sales amount :"))
+
+if(sale_amount>actual_cost):
+    profit=sale_amount-actual_cost
+    print(profit)
+else:
+    print("No profit")
