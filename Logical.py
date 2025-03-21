f962eed68c147df4ffee1ab7bf54a3ae7dee327c
TEJA271 <navatejtummalapally02@gmail.com>
2025-03-21 12:57:33 +0300

    Add files via upload

---
+++ Logical.py
@@ -0,0 +1,22 @@
+a=-1
+b=12
+
+if a>0 or b>0:
+    print("the numbers greater than 0")
+
+
+
+a=-1
+b=12
+
+if a>0 and b>0:
+    print("the numbers greater than 0")
+else:
+    print("One of the number is less than 0")
+
+a=0
+if not a:
+    print (True)
+else :
+    print(False)
+           
