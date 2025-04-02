f0fe4c17cdd28b9f81385fea63512afb7236fb22
TEJA271 <navatejtummalapally02@gmail.com>
2025-04-02 18:55:14 +0300

    Add files via upload

---
+++ Dvisible.py
@@ -0,0 +1,8 @@
+print("Enter a number (Numarator): ")
+numn = int(input())
+print("Enter a number (Denominator):")
+numd = int(input())
+if numn%numd==0:
+    print("it is divisble")
+else:
+    print("it is not divisble") 
