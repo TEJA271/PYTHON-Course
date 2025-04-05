27a81b27a11d5da7a719e436280a599e2e532137
TEJA271 <navatejtummalapally02@gmail.com>
2025-04-05 12:34:54 +0300

    Add files via upload

---
+++ Reverse a string.py
@@ -0,0 +1,8 @@
+string = input("please enter your own string :")
+
+string2 = ("")
+for n in string :
+    string2 = n+string2
+
+print("the Original string is",string)
+print("the reversed sting is ",string2)
