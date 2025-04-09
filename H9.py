3126c6ad05af37bae4da914a4da55e57b13f08a6
TEJA271 <navatejtummalapally02@gmail.com>
2025-04-09 20:16:46 +0300

    Add files via upload

---
+++ H9.py
@@ -0,0 +1,7 @@
+# 9. Write a Python program that asks for a word and then prints that word 5 times.
+
+word = input("Please enter a word: ")
+
+for _ in range(5):
+    print(word)
+    print("YEAY we printed the word five times")
