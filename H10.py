d545838620914371711f3e79b0070537db305aa2
TEJA271 <navatejtummalapally02@gmail.com>
2025-04-09 20:17:06 +0300

    Add files via upload

---
+++ H10.py
@@ -0,0 +1,6 @@
+# 10. Write a Python program that asks for your favorite animal and prints a sentence with it.
+
+favorite_animal = input("What is your favorite animal? : ")
+
+print("Your favorite animal is a ", favorite_animal )
+print(favorite_animal ,"is a cute animal")
