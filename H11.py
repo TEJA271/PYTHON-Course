4b78b44f2144847cdebdf59377fe02020e560892
TEJA271 <navatejtummalapally02@gmail.com>
2025-04-09 20:17:59 +0300

    Add files via upload

---
+++ H11.py
@@ -0,0 +1,7 @@
+# 11. Write a Python program that asks for a word and prints the word in reverse.
+
+c = int(input("enter the value of c :"))
+print("numbers from {0} to {1} are". format(c,1))
+for i in range(c,0,-1):
+    print(i)
+print("You got your answer")
