1bf86a073c9c048f5c0e0cfcb452fa9d67a006a8
TEJA271 <navatejtummalapally02@gmail.com>
2025-04-10 23:21:17 +0300

    Add files via upload

---
+++ H3.py
@@ -0,0 +1,5 @@
+# 3.Write a Python program that asks for your age and prints how old you will be next year.
+
+age = int(input("How old are you? : "))
+next_year_age = age + 1
+print("Next year, you will be", next_year_age, "years old!")
---
+++ H4.py
@@ -0,0 +1,4 @@
+# 4. Write a Python program that asks for your favorite color and prints a message with it.
+
+color = input("What is your favorite color? : ")
+print("Wow! " + color + " is a beautiful color!")
