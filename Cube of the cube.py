0f6efd1b706f20db24d09e20ee24f95d85662231
TEJA271 <navatejtummalapally02@gmail.com>
2025-05-02 12:58:39 +0300

    Add files via upload

---
+++ Cube of the cube.py
@@ -0,0 +1,10 @@
+def cube (number):
+    return number*number*number
+is dvisible by 3
+def by three (number):
+  if number %3 == 0:
+   return cube (number)
+  else:
+  return  False
+print(by_three(9))
+print(by_three(4))
