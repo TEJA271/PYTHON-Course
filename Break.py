7ae9ce8605e741aa5817f5941a5ebcb49ffa5005
TEJA271 <navatejtummalapally02@gmail.com>
2025-05-03 12:38:32 +0300

    Add files via upload

---
+++ Break.py
@@ -0,0 +1,17 @@
+a=input("enter a Word: ")
+for i in a :
+  if(i=="A"):
+    print("A found")
+    break
+  else:
+    print("A not found")
+
+
+#a = input ("enter a word :")
+#for i in a :
+ # if  (i == a):
+  #  print
+  #print("a is found")
+  #break
+ #else:
+ # print ("a is not found")
