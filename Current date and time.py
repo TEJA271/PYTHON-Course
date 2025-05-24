219c18c3a448c6e8b3b1a5d8f1be48afc66f48bd
TEJA271 <navatejtummalapally02@gmail.com>
2025-05-24 09:45:41 +0300

    Add files via upload

---
+++ Current date and time.py
@@ -0,0 +1,7 @@
+from datetime import  date , time ,datetime
+
+today = date.today()
+now = datetime.now()
+print ("todays date is",today)
+print("\nCurrent date and Time is ",now)
+print("\ndate components ",date.year , date.month , date.day)
