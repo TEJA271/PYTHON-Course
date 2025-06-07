7b20b964e12adc8dc4b923dd7a7f15958283a877
TEJA271 <navatejtummalapally02@gmail.com>
2025-06-07 12:57:34 +0300

    Add files via upload

---
+++ Operations on sets.py
@@ -0,0 +1,19 @@
+set1 = { 6 , 9 , 34 }
+print(set1)
+
+set2 = {1.0 , 'Hello', ( 6 , 9 , 34)}
+print(set2)
+
+set3 = { 1 , 12 , 56 , 67 , 82 , 78}
+print(set3)
+
+set4 = set([ 1 , 4 , 5 , 6 ])
+print(set4 , "\n")
+
+set5 = ([ 0 , 99 , 98 , 97 , 96 ])
+print("Original set : ")
+print (set5)
+set5.pop()
+print ("after removing the first element from the said set : ")
+
+print(set5 , "\n")
