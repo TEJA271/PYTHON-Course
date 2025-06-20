7fb388f74ff25d1b5514bb7e25cd3fc97d17d0f5
TEJA271 <navatejtummalapally02@gmail.com>
2025-06-20 13:02:47 +0300

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,9 @@
+class fruit:
+    def __init__( self , name , color ):
+      self.name = name
+      self.color = color
+Fruit1  = fruit("Strawberry" , 'red')
+print(Fruit1.name)
+
+Fruit2 = fruit("blackberry", 'black')
+print(Fruit2. name , color)
